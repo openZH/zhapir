4804b5d5fa1d7fb213c88039c7e14d4415f0109d
Tabea Eggler <tabea.eggler@hotmail.com>
2025-05-21 14:53:25 +0000

    added basic distribution class and create methode

--- playground.R
+++ playground.R
@@ -1,3 +1,5 @@
 devtools::load_all(".")
 
-create_dataset("Test", organisation_id = 14, description = "Such insights, much wow!")
+create_dataset("Hello Distribution", organisation_id = 14, description = "Such insights, much wow!")
+
+create_distribution(title = "Hello Distribution", dataset_id = 6819, description = "WOW this is a distribution!")
