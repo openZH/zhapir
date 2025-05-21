371b0a6069eee33cc88c8aeecb0a6210dce2e262
Christoph Zust <christoph.zust@statistik.ji.zh.ch>
2025-05-21 15:38:06 +0000

    feat: initialise text_arguments_parser

---
+++ R/text_arguments_parser.R
@@ -0,0 +1,13 @@
+get_all_organisations <- function(api_key){
+
+  # ls <-     httr2::request(paste0("https://mdv-dev.nebula.statzh.ch", "/api/v1/datasets")) |>
+  #   httr2::req_method("GET") |>
+  #   httr2::req_headers(
+  #     Accept         = "application/json, application/problem+json",
+  #     `x-api-key`    = "a44798a8-6ef7-42a1-8fff-beb3ea14c3bb"
+  #   ) |>
+  #   httr2::req_perform()
+
+
+
+}
