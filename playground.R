7e1cb058fef29bcc731b2293f45922e35b3c278b
philbosch <philippmartinbosch@gmail.com>
2025-05-23 14:46:42 +0000

    feat: add example for update functionality

--- playground.R
+++ playground.R
@@ -3,3 +3,5 @@ devtools::load_all(".")
 create_dataset("Hello Dataset 1", organisation_id = 14, description = "Such insights, much wow!")
 
 create_distribution(title = "Hello Distribution 1", dataset_id = 6819, description = "WOW this is a distribution!")
+
+update_dataset(id = 6798, title = "Wow this title is updated now!")
