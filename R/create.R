75e83bc088daecdb4c3f50fbaf015c79397ad0a1
philbosch <philippmartinbosch@gmail.com>
2025-05-23 13:26:18 +0000

    feat: add specific cli messages depending on distribution or dataset creation

--- R/create.R
+++ R/create.R
@@ -8,10 +8,24 @@ S7::method(create, Distribution) <- function(object, api_key, use_dev = TRUE) {
   post_request(object, "/api/v1/distributions", api_key, use_dev, object_label = "Distribution")
 }
 
-#' Helper to POST an object to the API and print message
+# Helper to POST an object to the API and print a CLI message
 post_request <- function(object, endpoint, api_key, use_dev = TRUE, object_label) {
   payload <- object_to_payload(object)
-  result <- api_request("POST", endpoint, payload, api_key, use_dev)
+  result  <- api_request("POST", endpoint, payload, api_key, use_dev)
 
-  cli::cli_alert_success("{.strong {object_label}} {.val {result$title}} created with ID {.val {result$id}}.")
+
+  if (identical(endpoint, "/api/v1/distributions")) {
+    # For distributions, include parent dataset ID
+    parent_id <- result$dataset$id
+    cli::cli_alert_success(
+      "{.strong {object_label}} {.val {result$title}} with ID {.val {result$id}} created inside Dataset with ID {.val {parent_id}}."
+    )
+  } else {
+    # For datasets (and all other endpoints), simple creation message (can be extended in the future)
+    cli::cli_alert_success(
+      "{.strong {object_label}} {.val {result$title}} created with ID {.val {result$id}}."
+    )
+  }
+
+  invisible(result)
 }
