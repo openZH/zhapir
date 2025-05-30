3d14ecc97de4b6796ea79bb45c4bd975d634fdaf
tknecht <thomas.knecht@statistik.ji.zh.ch>
2025-05-30 12:39:49 +0000

    construct Dataset with do.call and add a test parameter

--- R/create_dataset.R
+++ R/create_dataset.R
@@ -18,76 +18,50 @@
 #' @param see_also_ids      Optional integer vector
 #' @param api_key           API key (optional; falls back to env var)
 #' @param use_dev           Logical; use development base URL
+#' @param test              Defines if it is a test run. If TRUE, the
+#'                          dataset-object is returned into the environment.
+#'                          Default = FALSE
+#'
 #' @export
 create_dataset <- function(
     title,
     organisation_id,
-    description        = NULL,
-    contact_email      = NULL,
-    landing_page       = NULL,
-    issued             = NULL,
-    start_date         = NULL,
-    end_date           = NULL,
-    modified           = NULL,
-    modified_next      = NULL,
-    keyword_ids        = NULL,
+    description = NULL,
+    contact_email = NULL,
+    landing_page = NULL,
+    issued = NULL,
+    start_date = NULL,
+    end_date = NULL,
+    modified = NULL,
+    modified_next = NULL,
+    keyword_ids = NULL,
     zh_web_catalog_ids = NULL,
-    relation_ids       = NULL,
-    theme_ids          = NULL,
-    periodicity_id     = NULL,
-    see_also_ids       = NULL,
-    api_key            = NULL,
-    use_dev            = TRUE
-) {
-
-  print("Here")
+    relation_ids = NULL,
+    theme_ids = NULL,
+    periodicity_id = NULL,
+    see_also_ids = NULL,
+    api_key = NULL,
+    use_dev = TRUE,
+    test = FALSE) {
   # Extract or prompt for API key
-  #api_key <- get_api_key(api_key)
+  api_key <- get_api_key(api_key)
 
   # Title is required for creation
   if (is.null(title) || is.na(title) || nzchar(title) == FALSE) {
     stop("`title` ist erforderlich, um ein neues Dataset zu erstellen.", call. = FALSE)
   }
 
-
-
-  args <-as.list(match.call())
+  # Capture arguments of function call and construct a Dataset-Object
+  args <- as.list(match.call())
   args <- args[2:length(args)]
+  args <- args[!grepl("api_key|use_dev|test", names(args))]
 
-  date_args <- args[grepl("start_date|end_date|modified", names(args))]
+  ds <- do.call(Dataset, args)
 
-  args[grepl("start_date|end_date|modified", names(args))] <- NULL
-  date_args <- lapply(date_args, as.POSIXct)
-
-  ds <- do.call(Dataset, c(args, date_args))
-
-
-  # Prepare fields, converting NULL to appropriate NA or defaults
-  # FIXME: Where do we set defaults?
-  # if the defaults are set in the properties, all this is not needed but rather
-  # just pass the input parameters
-  # ds <- Dataset(
-  #   id = NA_real_,
-  #   title = title,
-  #   organisation_id = organisation_id,
-  #   description =  description,
-  #   contact_email = contact_email,
-  #   landing_page = landing_page,
-  #   issued = if (!is.null(issued)) as.POSIXct(issued, tz = "UTC") ,
-  #   start_date = if (!is.null(start_date)) as.POSIXct(start_date, tz = "UTC"),
-  #   end_date = if (!is.null(end_date)) as.POSIXct(end_date, tz = "UTC"),
-  #   modified = if (!is.null(modified)) as.POSIXct(modified, tz = "UTC"),
-  #   modified_next = if (!is.null(modified_next)) as.POSIXct(modified_next, tz = "UTC"),
-  #   keyword_ids = keyword_ids,
-  #   zh_web_catalog_ids = zh_web_catalog_ids,
-  #   relation_ids = relation_ids,
-  #   theme_ids = theme_ids,
-  #   periodicity_id = periodicity_id,
-  #   see_also_ids = see_also_ids
-  # )
-
-
-  return(ds)
   # Dispatch create method
-  #create(ds, api_key, use_dev)
+  if (!test) {
+    create(ds, api_key, use_dev)
+  } else {
+    return(ds)
+  }
 }
