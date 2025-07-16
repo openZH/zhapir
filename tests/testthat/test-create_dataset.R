53083d52b58408663a777a05b5f2439ff425550b
philbosch <philippmartinbosch@gmail.com>
2025-07-16 14:25:53 +0000

    fix: adapt tests to reflect changes in backend to issued & modified

--- tests/testthat/test-create_dataset.R
+++ tests/testthat/test-create_dataset.R
@@ -10,7 +10,6 @@ test_that("dataset object is correctly created by the create_dataset function", 
     organisation_id = 14,
     description = "Such insights, much wow!",
     theme_ids = c(1,2,3),
-    issued = "2025-03-31",
     test = TRUE
   )
 
@@ -19,7 +18,6 @@ test_that("dataset object is correctly created by the create_dataset function", 
     organisation_id = 14,
     description = "Such insights, much wow!",
     theme_ids = c(1,2,3),
-    issued = "2025-03-31",
   )
 
   expect_equal(ds, ds_test)
@@ -41,7 +39,6 @@ test_that("an error is returned if no title is set", {
     organisation_id = 14,
     description = "Such insights, much wow!",
     theme_ids = c(1,2,3),
-    issued = "2025-03-31",
     test = TRUE
   ),
   "`title` ist erforderlich"
